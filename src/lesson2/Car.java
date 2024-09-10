2f143e0fef9751a469a9b3804a394694362f12a2
Maxim Mihalishin <maxim.mihalishin@yandex.ru>
2024-09-10 11:43:03 +0300

    TASk-2-1-crate class of car

---
+++ src/lesson2/Car.java
@@ -0,0 +1,49 @@
+package lesson2;
+
+public class Car {
+    private String model, license, color;
+    private int year;
+
+    public Car(String model, String license, String color, int year) {
+        this.model = model;
+        this.license = license;
+        this.color = color;
+        this.year = year;
+    }
+
+    public Car() {
+
+    }
+
+    public Car(String model, int year) {
+        this.model = model;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", license='" + license + '\'' +
+                ", color='" + color + '\'' +
+                ", year=" + year +
+                '}';
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+}
